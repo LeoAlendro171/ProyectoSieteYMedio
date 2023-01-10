f6e006449864b34f5c07362cf9fb560447a3092d
LeoAlendro171 <leon_alejandro_github@gmx.es>
2023-01-10 19:21:34 +0100

    otro commit

--- mis_funciones/funciones.py
+++ mis_funciones/funciones.py
@@ -26,15 +26,20 @@ def addRemovePlayers():
     option = getOpt(textOpts, inputOptText, option_range, exception)
     return option
 
-def setNewPlayer(human=True):
+def newPlayer(dni, name, profile, human):
+    player_data = (dni, {"name": name, "human": human, "bank": False, "initialCard": "", "priority": 0,
+                         "type": profile, "bet": 4, "points": 0, "cards": [], "roundPoints": 0})
+    return player_data
+
+def setNewPlayer(human = True):
     while True:
         try:
             name = input(datos.space+"Name: ")
             if not name.isalpha():
                 raise ValueError(datos.space + "Incorrect name, enter a name that is not empty with only letters")
-            nif = input(datos.space+"NIF: ")
-            if not len(nif) == 9 or not nif[:8].isdigit() or not nif[8].isalpha() \
-               or not datos.letrasDni[int(nif[:8])%23] == nif[8].upper():
+            dni = input(datos.space+"NIF: ")
+            if not len(dni) == 9 or not dni[:8].isdigit() or not dni[8].isalpha() \
+               or not datos.letrasDni[int(dni[:8])%23] == dni[8].upper():
                 raise ValueError(datos.space + "Wrong NIF")
             textOps = datos.space+"Select your Profile:\n"+\
                                         datos.space+"1)Cautious\n"+\
@@ -44,8 +49,14 @@ def setNewPlayer(human=True):
             option_range = [1,2,3]
             exception = []
             option = getOpt(textOps,inputOptText,option_range,exception)
-            return option
+            if option == 1:
+                profile = 30
+            elif option == 2:
+                profile = 40
+            elif option == 3:
+                profile = 50
         except ValueError as e:
             print(e)
             input(datos.space+"Enter to continue")
             clear()
+
