b64c9dd04b52dd89bb84ffeb59f14595a00b5f58
LeoAlendro171 <leon_alejandro_github@gmx.es>
2023-01-10 16:28:47 +0100

    primer commit

---
+++ SieteYMedio.py
@@ -0,0 +1,93 @@
+from mis_funciones import funciones
+from mis_funciones import datos
+
+
+while not datos.Salir:
+    while datos.flg_00:
+        textOpts = datos.space + "1)Add/Remove/Show Players" + "\n" + datos.space + "2)Settings" + \
+                "\n" + datos.space + "3)Play Game" + "\n" + datos.space + "4)Ranking" + "\n" + datos.space + \
+                "5)Reports" + "\n" + datos.space + "6)Exit"
+        inputOptText = datos.space + "Option: "
+        option_range = [1,2,3,4,5]
+        exception = [6]
+        option = funciones.getOpt(textOpts,inputOptText,option_range,exception)
+        if option == 1:
+            datos.flg_00 = False
+            datos.flg_01 = True
+        elif option == 2:
+            datos.flg_00 = False
+            datos.flg_02 = True
+        elif option == 3:
+            print("Hola")
+        elif option == 4:
+            datos.flg_00 = False
+            datos.flg_04 = True
+        elif option == 5:
+            datos.flg_00 = False
+            datos.flg_05 = True
+        elif option == 6:
+            datos.flg_00 = False
+            datos.Salir = True
+    while datos.flg_01:
+        funciones.clear()
+        textOpts = datos.space + "1)New Human Player" + "\n" + datos.space +"2)New Bot" + \
+                   "\n" + datos.space + "3)Show/Remove Players"+ "\n" + datos.space + "4)Go Back"
+        inputOptText = datos.space + "Option: "
+        option_range = [1,2,3,4]
+        exception = [5]
+        option = funciones.getOpt(textOpts,inputOptText,option_range,exception)
+        if option == 1:
+            funciones.clear()
+            funciones.setNewPlayer()
+        elif option == 4:
+            funciones.clear()
+            datos.flg_01 = False
+            datos.flg_00 = True
+    while datos.flg_02:
+        funciones.clear()
+        textOpts = datos.space + "1)Set Game Players"+ "\n" + datos.space + "2)Set Card's Deck" + \
+                   "\n" + datos.space + "3)Set Max Rounds (Default 5 Rounds)" + "\n" + datos.space + "4)Go Back"
+        inputOptText = datos.space + "Option: "
+        option_range = [1,2,3,4]
+        exception = [5]
+        option = funciones.getOpt(textOpts,inputOptText,option_range,exception)
+        if option == 4:
+            funciones.clear()
+            datos.flg_02 = False
+            datos.flg_00 = True
+    while datos.flg_04:
+        funciones.clear()
+        textOpts = datos.space + "1)Players With More Earnings" + "\n" + datos.space + "2)Players With More Games Played" + \
+                   "\n" + datos.space + "3)Players With More Minutes Played" + "\n" + datos.space + "4)Go Back"
+        inputOptText = datos.space + "Option: "
+        option_range = [1, 2, 3, 4]
+        exception = [5]
+        option = funciones.getOpt(textOpts, inputOptText, option_range, exception)
+        if option == 4:
+            funciones.clear()
+            datos.flg_04 = False
+            datos.flg_00 = True
+    while datos.flg_05:
+        funciones.clear()
+        textOpts = datos.space + "1)Initial card more repeated by each user,\n" + datos.space + \
+                   "only users who have played a minimum of 3 games"\
+                   + "\n" + datos.space + "2)Player who makes the highest bet per game,\n" + datos.space + \
+                   "find the round with the highest bet" +\
+                   "\n" + datos.space + "3)Player who makes the lowest bet per game"\
+                   + "\n" + datos.space + "4)Percentage of rounds won per player in each game\n" + datos.space + " (%), " \
+                         "as well as their average bet for the game"\
+                   + "\n" + datos.space + "5)List of games won by Bots"\
+                   + "\n" + datos.space + "6)Rounds won by the bank in each game"\
+                   + "\n" + datos.space + "7)Number of users that have been the bank in each game"\
+                   + "\n" + datos.space + "8)Average bet per game"\
+                   + "\n" + datos.space + "9)Average bet of the first round of each game"\
+                   + "\n" + datos.space + "10)Average bet of the last round of each game"\
+                   + "\n" + datos.space + "11)Go back"
+        inputOptText = datos.space + "Option: "
+        option_range = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        exception = [11]
+        option = funciones.getOpt(textOpts, inputOptText, option_range, exception)
+        if option == 11:
+            funciones.clear()
+            datos.flg_05 = False
+            datos.flg_00 = True
---
+++ mis_funciones/funciones.py
@@ -0,0 +1,38 @@
+from . import datos
+
+def clear():
+    print("\033[H\033[J", end="")
+
+def getOpt(textOpts="", inputOptText="", rangeList=[], exceptions=[]):
+    print(textOpts)
+    while True:
+        try:
+            option = input(inputOptText)
+            if option.isdigit():
+                option = int(option)
+            if option not in rangeList and option not in exceptions:
+                raise ValueError("Invalid option".center(140,"="))
+            else:
+                return option
+        except ValueError as e:
+            print(e)
+            input(" "*58+"Press enter to continue")
+
+def setNewPlayer(human=True):
+    while True:
+        try:
+            name = input(datos.space+"Name: ")
+            if not name.isalpha():
+                raise ValueError(datos.space + "Incorrect name, enter a name that is not empty with only letters")
+            nif = input(datos.space+"NIF: ")
+            if not len(nif) == 9 or not nif[:8].isdigit() or not nif[8].isalpha() \
+               or not datos.letrasDni[int(nif[:8])%23] == nif[8].upper():
+                raise ValueError(datos.space + "Wrong NIF")
+            textOps = datos.space+"Select your Profile:\n"
+                                        "1)Cautious\n"
+                                        "2)Moderated\n"
+                                        "3)Bold\n"
+            profile = getOpt()
+        except ValueError as e:
+            print(e)
+            input(datos.space+"Enter to continue")
